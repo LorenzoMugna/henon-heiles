2d06f91b9937d85940292e217f9caa68bf242255
LorenzoMugna <lorenzo.mugnaiolil@gmail.com>
2025-03-25 03:01:27 +0100

    ultimi ritocchi

--- matlab/studio_potenziale.m
+++ matlab/studio_potenziale.m
@@ -1,17 +1,14 @@
-syms q_x q_y p_x p_y
+run('funzioni_comuni.m');
+nablaU = gradient(U, [qx, qy])
+HU = jacobian(nablaU, [qx, qy])
 
-U = 1/2*(q_x^2 + q_y^2) + q_x^2 * q_y - 1/3*q_y^3;
+punti_equilibrio = solve(nablaU==[0,0], [qx,qy], 'ReturnConditions', true);
 
-nablaU = gradient(U, [q_x, q_y])
-HU = jacobian(nablaU, [q_x, q_y])
+[punti_equilibrio.qx, punti_equilibrio.qy]
 
-punti_equilibrio = solve(nablaU==[0,0], [q_x,q_y], 'ReturnConditions', true);
-
-[punti_equilibrio.q_x, punti_equilibrio.q_y]
-
-HU_f = matlabFunction(HU, 'vars', {q_x, q_y});
-for i = 1:size(punti_equilibrio.q_x,1)
-	Hu_i = HU_f(punti_equilibrio.q_x(i), punti_equilibrio.q_y(i))
+HU_f = matlabFunction(HU, 'vars', {qx, qy});
+for i = 1:size(punti_equilibrio.qx,1)
+	Hu_i = HU_f(punti_equilibrio.qx(i), punti_equilibrio.qy(i))
 	lambda = eig(Hu_i);
 	if all(lambda > 0)
 		disp('>0')
