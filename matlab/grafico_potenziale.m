1c4014f8f09a3d0fd862ca6c6225b2e50d2521db
LorenzoMugna <lorenzo.mugnaiolil@gmail.com>
2025-03-25 01:08:02 +0100

    In fase di aggiunta codice sorgente

--- matlab/grafico_potenziale.m
+++ matlab/grafico_potenziale.m
@@ -5,7 +5,12 @@ T = px^2 + py^2;
 U = 1/2*(qx^2 + qy^2) + qx^2 * qy - 1/3*qy^3;
 U_func = matlabFunction(U, 'vars', {[qx; qy]});
 
-
+Hamiltonian = T + U;
+Jinv = [zeros(2), eye(2); -eye(2), zeros(2)];
+NablaH = gradient(Hamiltonian, [qx, qy, px, py]);
+F = Jinv * NablaH;
+F = F(3:4);
+f = matlabFunction(F, 'vars', {[qx; qy]});
 % Creazione grafico
 figure(1)
 fsurf(@(qx, qy) U_func([qx;qy]), [-3, 3, -3, 3])
@@ -33,3 +38,23 @@ xlabel('q_x')
 ylabel('q_y')
 contour(X, Y, Z, [1/6,1/6], 'LineColor', 'r')
 plot(0,0, 'r.')
+
+%% animazione simulazione sul grafico appena disegnato
+H = 1/8;
+qx=0;
+qy=1/2;
+py=0.2;
+px=sqrt(2*(H-U_func([qx;qy]))-py^2);
+p = [px; py];
+q = [qx; qy];
+p_handle = plot(q(1), q(2), 'o', 'MarkerSize', 5, 'MarkerFaceColor', 'r')
+while true
+	for i = 1:20
+		[p, q] = stormerverlet(p,q, 0.01, f);
+	end
+	set(p_handle, 'XData', qfrom(1), 'YData', q(2))
+	drawnow
+	if qx < 0
+		break
+	end
+end
--- matlab/implEuler.m
+++ matlab/implEuler.m
@@ -1,36 +1,23 @@
 function [p, q] = implEuler(pin, qin, h, f)
-    % Implicit Euler method using fixed-point iteration
-    % pin: initial momentum
-    % qin: initial position
-    % h: time step
-    % f: function handle for the force (gradient of the potential)
-
-    % Initialize q and p
     q = qin;
     p = pin;
 
-    % Tolerance and maximum iterations for convergence
-    tol = 1e-8;
+    tol = 1e-10;
     max_iter = 100;
 
-    % Fixed-point iteration
     for iter = 1:max_iter
-        % Store the previous values for convergence check
         q_prev = q;
         p_prev = p;
 
-        % Update q and p using the implicit Euler equations
-        q = qin + h * p;
+        q = qin + h * p + h^2/2 * f(q);
         p = pin + h * f(q);
 
-        % Check for convergence
         if norm(q - q_prev, 2) < tol && norm(p - p_prev, 2) < tol
             break;
         end
     end
 
-    % Check if the iteration converged
     if iter == max_iter
-        warning('Implicit Euler did not converge within the maximum number of iterations.');
+        warning('Eulero implicito non ha converso nel numero massimo di iterazioni');
     end
 end
